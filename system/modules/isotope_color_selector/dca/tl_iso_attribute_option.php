eeea4edd35de80fe6c0c690cbb40077a7395db21
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-09-02 14:18:52 -0400

    Update tl_iso_attribute_option.php

--- system/modules/isotope_color_selector/dca/tl_iso_attribute_option.php
+++ system/modules/isotope_color_selector/dca/tl_iso_attribute_option.php
@@ -1,24 +1,24 @@
 <?php
 
 /**
- * Bright Cloud Studio's Add User Fields
+ * Bright Cloud Studio's Isotope Color Selector
  *
- * Copyright (C) 2021 Bright Cloud Studio
+ * Copyright (C) 2022 Bright Cloud Studio
  *
- * @package    bright-cloud-studio/add-user-fields
+ * @package    bright-cloud-studio/isotope-color-selector
  * @link       https://www.brightcloudstudio.com/
  * @license    http://opensource.org/licenses/lgpl-3.0.html
 **/
 
  /* Extend the tl_user palettes */
-foreach ($GLOBALS['TL_DCA']['tl_user']['palettes'] as $k => $v) {
-    $GLOBALS['TL_DCA']['tl_user']['palettes'][$k] = str_replace('email;', 'email;{add_user_fields_legend},user_image,user_image_size,user_image_meta,user_bio;', $v);
+foreach ($GLOBALS['TL_DCA']['tl_iso_attribute_option']['palettes'] as $k => $v) {
+    $GLOBALS['TL_DCA']['tl_iso_attribute_option']['palettes'][$k] = str_replace('label;', 'label;{color_legend},color_css,color_image;', $v);
 }
 
 /* Add fields to tl_user */
-$GLOBALS['TL_DCA']['tl_user']['fields']['user_image'] = array
+$GLOBALS['TL_DCA']['tl_iso_attribute_option']['fields']['color_image'] = array
 (
-	'label'                   => &$GLOBALS['TL_LANG']['tl_user']['user_image'],
+	'label'                   => &$GLOBALS['TL_LANG']['tl_user']['color_image'],
 	'inputType'               => 'fileTree',
 	'default'		  => '',
 	'search'                  => true,
@@ -28,4 +28,15 @@ $GLOBALS['TL_DCA']['tl_user']['fields']['user_image'] = array
 		'filesOnly' => true
 	],
 	'sql'                    => ['type' => 'binary', 'length' => 16, 'notnull' => false, 'fixed' => true]
+);
+
+$GLOBALS['TL_DCA']['tl_iso_attribute_option']['fields']['color_css'] = array
+(
+	'label'			=> &$GLOBALS['TL_LANG']['tl_user']['color_css'],
+	'inputType'		=> 'textarea',
+	'eval'                	=> [
+		'rte'=>'tinyMCE',
+		'tl_class'=>'long'
+	],
+	'sql'                   => "mediumtext NOT NULL default ''"
 );
