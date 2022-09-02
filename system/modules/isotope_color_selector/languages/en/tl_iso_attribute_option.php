9b42812e4c1e371aed44e86b6cc76b6b2cc0f3fa
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-09-02 14:20:19 -0400

    Update tl_iso_attribute_option.php

--- system/modules/isotope_color_selector/languages/en/tl_iso_attribute_option.php
+++ system/modules/isotope_color_selector/languages/en/tl_iso_attribute_option.php
@@ -12,10 +12,8 @@
 
 
 /* Fields */
-$GLOBALS['TL_LANG']['tl_user']['user_image']          = array('Image', 'Custom "Image" User field.');
-$GLOBALS['TL_LANG']['tl_user']['user_image_size']     = array('Image Size', 'Pick a size for the user\'s image.');
-$GLOBALS['TL_LANG']['tl_user']['user_image_meta']     = array('Meta Data', 'Input meta data for the user\'s image');
-$GLOBALS['TL_LANG']['tl_user']['user_bio']            = array('Bio', 'Custom "Bio" User field.');
+$GLOBALS['TL_LANG']['tl_iso_attribute_option']['color_image']   = array('Color Image', 'Custom "Image" for a color.');
+$GLOBALS['TL_LANG']['tl_iso_attribute_option']['color_css']     = array('Color CSS', 'CSS class of the color.');
 
 /* Legends */
-$GLOBALS['TL_LANG']['tl_user']['add_user_fields_legend'] = 'Custom Fields\'s Settings';
+$GLOBALS['TL_LANG']['tl_iso_attribute_option']['color_legend'] = 'Custom Color Settings';
