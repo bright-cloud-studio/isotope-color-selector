afa0d097285758fb9e40647d2902c4d8c29eb22b
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-09-29 11:42:59 -0400

    Create config.php

---
+++ system/modules/isotope_color_selector/config/config.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Bright Cloud Studio - Isotope Color Selector
+ *
+ * Copyright (C) 2022 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/isotope-color-selector
+ * @link       http://brightcloudstudio.com
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ */
+
+/* Hooks */
+if (\Config::getInstance()->isComplete()) {
+  $GLOBALS['TL_HOOKS']['replaceInsertTags'][] = array('CustomTags\AddColorSelector', 'onReplaceTag');
+}
