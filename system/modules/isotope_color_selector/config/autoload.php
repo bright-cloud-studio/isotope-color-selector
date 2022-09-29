bf84293e77106273849a38d0808a4284c98b9f91
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-09-29 11:42:07 -0400

    Create autoload.php

---
+++ system/modules/isotope_color_selector/config/autoload.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Bright Cloud Studio - Isotope Color Selector
+ *
+ * Copyright (C) 2022 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/isotope-color-selector
+ * @link       http://brightcloudstudio.com
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ */
+
+ClassLoader::addClasses(array
+(
+  'CustomTags\AddVariantsTags' 			=> 'system/modules/isotope_list_products_variants/library/custom_tags/AddColorSelector.php'
+));
