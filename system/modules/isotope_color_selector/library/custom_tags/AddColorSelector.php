8e4810929cd07fb44846f3dfa368a6390f40c3b2
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-09-29 11:45:13 -0400

    Update AddColorSelector.php

--- system/modules/isotope_color_selector/library/custom_tags/AddColorSelector.php
+++ system/modules/isotope_color_selector/library/custom_tags/AddColorSelector.php
@@ -22,7 +22,7 @@ class AddVariantsTags extends \System
 	{
 		// if this tag doesnt contain :: it doesn't have an id, so we can stop right here
 		if (stristr($insertTag, "::") === FALSE) {
-			return 'Your tag has no ID. Please add a User ID or remove this tag from the page.';
+			return 'Your tag has no ID. Please add a Product ID or remove this tag from the page.';
 		}
 
 		// break our tag into an array
@@ -30,46 +30,8 @@ class AddVariantsTags extends \System
 
 		// lets make decisions based on the beginning of the tag
 		switch($arrTag[0]) {
-			// if the tag is what we want, {{simple_inventory::id}}, then lets go
-			case 'variants_dimentions':
-				$dbObj = \Database::getInstance()->prepare("SELECT * FROM tl_iso_product WHERE pid = '" . $arrTag[1] . "' AND published = 1 ORDER BY cast(wp_size as decimal(7,2)) ASC")->execute();  
-				
-				$buffer = '';
-				if ($dbObj->numRows > 0)
-				{
-					$arrLocation = array(
-						'id'		=> 111,
-						'pid'		=> 222
-					);
-				    while($dbObj->next()) {
-    				    
-    					$arrLocation['id'] 		= $dbObj->id;
-    					$arrLocation['pid']		= $dbObj->pid;
-    					$arrLocation['sku']		= $dbObj->sku;
-    					$arrLocation['wp_size']		= $dbObj->wp_size;
-    					
-    					$arrLocation['baseprice']	= 999;
-    					
-    					
-    					// Lets get our correct prices
-    					
-    					// First, search tl_iso_product_price using this products ID and that fields PID
-    					$step1 = \Database::getInstance()->prepare("SELECT * FROM tl_iso_product_price WHERE pid = '" . $dbObj->id . "'")->execute();  
-    					while($step1->next()) {
-    					    // Then, search tl_iso_product_pricetier using the PID from the last query to get the 'price'
-    					    $step2 = \Database::getInstance()->prepare("SELECT * FROM tl_iso_product_pricetier WHERE pid = '" . $step1->id . "'")->execute();  
-					        while($step2->next()) {
-					            // Finally, assign that price to our templates array
-					            $arrLocation['baseprice'] = $step2->price;
-					        }
-    					}
-    					
-    					$template = new FrontendTemplate('item_variant_dimentions');
-    					$template->variant = $arrLocation;
-    					$buffer .= $template->parse();
-				    }
-					return $buffer;
-				}
+			case 'color_selector':
+				echo "COLOR SELECTOR FOR PRODUCT: " . $arrTag[1];
 			break;
 		}
 
