cdb801b9168f16442409a85a94128ff6e38c7739
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-09-08 09:41:03 -0400

    Update tl_iso_attribute_option.php

--- system/modules/isotope_color_selector/languages/en/tl_iso_attribute_option.php
+++ system/modules/isotope_color_selector/languages/en/tl_iso_attribute_option.php
@@ -11,9 +11,10 @@
 **/
 
 
-/* Fields */
-$GLOBALS['TL_LANG']['tl_iso_attribute_option']['color_image']   = array('Color Image', 'Custom "Image" for a color.');
-$GLOBALS['TL_LANG']['tl_iso_attribute_option']['color_css']     = array('Color CSS', 'CSS class of the color.');
-
 /* Legends */
 $GLOBALS['TL_LANG']['tl_iso_attribute_option']['color_legend'] = 'Custom Color Settings';
+
+/* Fields */
+$GLOBALS['TL_LANG']['tl_iso_attribute_option']['color_css']          = array('Color CSS', 'CSS class of the color.');
+$GLOBALS['TL_LANG']['tl_iso_attribute_option']['color_image']        = array('Color Image', 'Custom "Image" for a color.');
+$GLOBALS['TL_LANG']['tl_iso_attribute_option']['color_image_size']   = array('Color Image Size', 'Select the Image Size for our color image.');
